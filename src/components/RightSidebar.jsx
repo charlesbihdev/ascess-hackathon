6228d8d1d1d5124d35ac8b991d6523a79577258e
Michael Forson <micfor722@gmail.com>
2024-08-13 10:31:18 +0000

    rightside updated with events

--- src/components/RightSidebar.jsx
+++ src/components/RightSidebar.jsx
@@ -2,6 +2,7 @@
 
 import { useState } from "react";
 import Link from "next/link";
+import EventCard from "./EventCard";
 
 import { MdOutlineDashboard } from "react-icons/md";
 import { RiAdminLine } from "react-icons/ri";
@@ -26,21 +27,57 @@ const RightSidebar = ({ className, showRightSidebar }) => {
         showRightSidebar ? "translate-x-0" : "translate-x-full"
       } !bg-white xl:translate-x-0 dark:bg-gray-800 ${className}`}
     >
-      <div className="text-black font-bold m-4">
+      <div className="text-black font-bold m-4 my-3 mb-3">
         <h2 className="mb-3">Trending</h2>
         <div className="flex justify-around">
           <div className="bg-[url('/images/story/story102.jpg')] bg-cover bg-center flex flex-col justify-end  h-36 bg-slate-400 w-2/5 rounded-2xl overflow-hidden ">
             <div className="flex bg-white mx-auto gap-1 p-1 m-1 rounded-xl font-normal text-sm ">
-              <span className="bg-slate-200 rounded-full w-5 "></span>
+              <span className="bg-[url('/images/avatar/profile2.jpg')] bg-cover bg-center bg-slate-200 rounded-full w-5 "></span>
               <p>Charles</p>
             </div>
           </div>
           <div className="bg-[url('/images/story/storyy101.jpeg')] bg-cover bg-center flex flex-col justify-end  h-36 bg-slate-400 w-2/5 rounded-2xl overflow-hidden">
             <div className="flex bg-white mx-auto gap-1 p-1 m-1 rounded-xl font-normal text-sm ">
-              <span className="bg-slate-200 rounded-full w-5 "></span>
+              <span className="bg-[url('/images/avatar/profile1.jpeg')] bg-cover bg-center bg-slate-200 rounded-full w-5 "></span>
               <p>Aceses</p>
             </div>
           </div>
+        </div>
+        
+        <h2 className="my-3">Events</h2>
+        <div className="flex flex-col">
+          {/* Example 3: Research Paper Presentation */}
+          <EventCard
+            date="12"
+            month="Aug"
+            time="2:00 PM"
+            eventTitle="ACESES Hackathon"
+            address="CL1"
+          />
+          {/* Example 1: Annual Tech Symposium */}
+          <EventCard
+            date="15"
+            month="Aug"
+            time="10:00 AM"
+            eventTitle="Tech Symposium 2024"
+            address="Mini Auditorium"
+          />
+
+          {/* Example 2: Freshers' Orientation */}
+          <EventCard
+            date="5"
+            month="Sept"
+            time="9:00 AM"
+            eventTitle="Freshers' Orientation 2024"
+            address="Main auditorium"
+          />
+          <EventCard
+            date="18"
+            month="Nov"
+            time="3:00 PM"
+            eventTitle="Robotics Club"
+            address="LH3"
+          />
         </div>
       </div>
     </aside>
