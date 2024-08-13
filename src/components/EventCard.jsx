ce96eeaf8157b7a25784e689d34f7bd3fcb90569
Nadone1 <127548426+Nadone1@users.noreply.github.com>
2024-08-13 18:09:45 +0000

    changes made

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -11,7 +11,7 @@ const EventCard = ({
 }) => {
   return (
     <div className="w-full h-14  rounded-lg border flex flex-row shadow-md  mb-5">
-      <div className="bg-blue-600 rounded-lg w-1/4 py-2 flex items-center justify-center shadow-inner">
+      <div className="bg-red-500 rounded-lg w-1/4 py-2 flex items-center justify-center shadow-inner">
         <div className="text-center ">
           <div className="text-white font-bold text-lg">{date}</div>
           <div className="text-white font-normal text-sm">{month}</div>
